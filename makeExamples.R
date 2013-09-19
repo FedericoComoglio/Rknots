5ba2c9f1fc0f952912b8106e89a44aeb82c6158c
FedericoComoglio <federico.comoglio@gmail.com>
2013-09-19 18:37:22 +0200

    Uploaded source code and vignette

---
+++ makeExamples.R
@@ -0,0 +1,27 @@
+##Script generated in:
+# 2011
+# 9:55:08 PM
+#by: 
+# Author: Federico Comoglio @ D-BSSE, ETH Zurich
+###############################################################################
+
+#makeExampleKnot (k governs the output. k = TRUE returns a knot, FALSE returns a link.
+makeExampleKnot <- function(k = TRUE) {
+	if(k) {
+		Rolfsen.table <- NULL; rm(Rolfsen.table)
+		data(Rolfsen.table, package='Rknots')
+		knot <- Rolfsen.table[[ sample(1 : length(Rolfsen.table), size = 1) ]]
+		return( knot )
+	}
+	else {
+		link.table <- NULL; rm(link.table)
+		data(link.table, package='Rknots')
+		link <- link.table[[ sample(1 : length(link.table), size = 1) ]]
+		return( link )
+	}
+}
+
+makeExampleProtein <- function() {
+	protein <- loadProtein( system.file("extdata/2k0a.pdb", package="Rknots") )
+	return(protein)
+}
